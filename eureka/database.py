5d5d69e3111c8fbe411b5ff3167a61139fa4d045
Samuel Hoffstaetter <samuel@hoffstaetter.com>
2010-02-04 21:02:39 -0500

    sqlite database's umask matches login user's umask

--- eureka/database.py
+++ eureka/database.py
@@ -1,3 +1,5 @@
+import os
+
 def connect(database):
     ''' connects to an sqlite database with given file name '''
 
@@ -10,12 +12,17 @@ def connect(database):
             return None
 
     if database != ':memory:':
-        from os import path
-        dir = path.dirname(__file__)
-        eureka_base, _ = path.split(dir)
-        database = path.join(eureka_base, database)
+        dir = os.path.dirname(__file__)
+        eureka_base, _ = os.path.split(dir)
+        database = os.path.join(eureka_base, database)
 
     connection = sqlite.connect(database)
     connection.text_factory = str
+
+    if database != ':memory:':
+        umask = os.umask(0) # unfortunately, os.umask will also set to umask
+        os.umask(umask)     # so we need to reset it
+
+        os.chmod(database, 0o666 & ~umask)
 
     return connection
