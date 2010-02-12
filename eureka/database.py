d39996a790186e1724b3780dff28cbd25f0da46c
Jay Searson <jay.searson@gmail.com>
2010-02-12 11:52:34 -0500

    Changing check for whether database is in memory. Not sure this is correct.

--- eureka/database.py
+++ eureka/database.py
@@ -11,18 +11,15 @@ def connect(database):
         except ImportError:
             return None
 
-    is_new_file = False
-
     if database != ':memory:':
         dir = os.path.dirname(__file__)
         eureka_base, _ = os.path.split(dir)
         database = os.path.join(eureka_base, database)
-        is_new_file = not os.path.isfile(database)
 
     connection = sqlite.connect(database)
     connection.text_factory = str
 
-    if is_new_file:
+    if database != ':memory:':
         umask = os.umask(0) # unfortunately, os.umask will also set to umask
         os.umask(umask)     # so we need to reset it
 
